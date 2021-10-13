efc8d0a6e7c8fbedb498245bfff0850a9b10fe4b
sgold8751 <sgold8751@gmail.com>
2021-10-13 12:21:10 -0400

    Update CodeV1.ino

--- CodeV1.ino
+++ CodeV1.ino
@@ -4,42 +4,27 @@
 
 //creating objects
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //this may need to move, may need reconfiguring
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1); //motor from port 1
+Adafruit_DCMotor *Left = AFMS.getMotor(1); //motor from port 1
+Adafruit_DCMotor *Right = AFMS.getMotor(2); //motor from port 2
 
   int vSpeed = 110;        // MAX 255
-  int turn_speed = 230;    // MAX 255 
+  int turn_speed = 230;    // MAX 255, for wheel on inside of turn 
   int turn_delay = 10;
-  int n=500;               //sensor value for yes/no line detection
-
+  int n1=500;              //sensor1 value for yes/no line detection
+  int n2=500               //sensor2 value for yes/no line detection
 
 //Motor shield commands
-//myMotor->setSpeed(150); speed set 0-255
-//myMotor->run(FORWARD); inputs FORWARD, BACKWARD, RELEASE
-
-
-  
-//L293 Connection   (This may be unnessesary as we are using a defferent motor shield)
-  const int motorA1      = 8;  
-  const int motorA2      = 10; 
-  const int motorAspeed  = 9;
-  const int motorB1      = 12; 
-  const int motorB2      = 13; 
-  const int motorBspeed  = 11;
+//myMotor->setSpeed(150); //speed set 0-255
+//myMotor->run(FORWARD); //inputs FORWARD, BACKWARD, RELEASE
 
 //Sensor Connection
   const int left_sensor_pin =A0;
   const int right_sensor_pin =A1;
 
-  
-  
   int left_sensor_state;
   int right_sensor_state;
 
 void setup() {
-  pinMode(motorA1, OUTPUT);
-  pinMode(motorA2, OUTPUT);
-  pinMode(motorB1, OUTPUT);
-  pinMode(motorB2, OUTPUT);
 
   Serial.begin(9600);
   AFMS.begin();  //begins motor shield
@@ -54,56 +39,56 @@ void loop() {
 left_sensor_state = analogRead(left_sensor_pin);
 right_sensor_state = analogRead(right_sensor_pin);
 
-if(right_sensor_state > n && left_sensor_state < n)
+Serials();
+
+if(right_sensor_state > n1 && left_sensor_state < n2)
 {
   Serial.println("turning right");
 
-  digitalWrite (motorA1,LOW);
-  digitalWrite(motorA2,HIGH);                       
-  digitalWrite (motorB1,LOW);
-  digitalWrite(motorB2,HIGH);
+  Left->run(FORWARD);
+  Right->run(FORWARD);
 
-  analogWrite (motorAspeed, vSpeed);
-  analogWrite (motorBspeed, turn_speed);
+  Left->setSpeed(vSpeed); //speed set Left
+  Right->setSpeed(turn_speed); //turn set Right
+
   
   }
-if(right_sensor_state < n && left_sensor_state > n)
+if(right_sensor_state < n1 && left_sensor_state > n2)
 {
   Serial.println("turning left");
   
-  digitalWrite (motorA1,HIGH);
-  digitalWrite(motorA2,LOW);                       
-  digitalWrite (motorB1,HIGH);
-  digitalWrite(motorB2,LOW);
+  Left->run(FORWARD);
+  Right->run(FORWARD);
 
-  analogWrite (motorAspeed, turn_speed);
-  analogWrite (motorBspeed, vSpeed);
+  Right->setSpeed(vSpeed); //speed set Right
+  Left->setSpeed(turn_speed); //turn set Left
 
   delay(turn_delay);
   }
 
-if(right_sensor_state > n && left_sensor_state > n)
+if(right_sensor_state > n1 && left_sensor_state > n2)
 {
   Serial.println("going forward");
 
-  digitalWrite (motorA2,LOW);
-  digitalWrite(motorA1,HIGH);                       
-  digitalWrite (motorB2,HIGH);
-  digitalWrite(motorB1,LOW);
-
-  analogWrite (motorAspeed, vSpeed);
-  analogWrite (motorBspeed, vSpeed);
+  Left->run(FORWARD);
+  Right->run(FORWARD);
+  
+  Left->setSpeed(vSpeed); //speed set Left
+  Right->setSpeed(vSpeed); //speed set Right
 
   delay(turn_delay);
   
   }
 
-if(right_sensor_state < n && left_sensor_state < n)
+if(right_sensor_state < n1 && left_sensor_state < n2)
 { 
   Serial.println("stop");
+
+  Left->run(RELEASE);
+  Right->run(RELEASE);
   
-  analogWrite (motorAspeed, 0);
-  analogWrite (motorBspeed, 0);
+  Left->setSpeed(0); //speed set Left, stop
+  Right->setSpeed(0); //speed set Right, stop
   
   }
 
