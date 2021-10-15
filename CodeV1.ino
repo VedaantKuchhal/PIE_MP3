d9b8511da551323f7b6c7bf9176db990e49116d4
VedaantKuchhal <vedaantk22@gmail.com>
2021-10-15 11:20:06 -0400

    Working code for successful track run!

--- CodeV1.ino
+++ CodeV1.ino
@@ -7,11 +7,11 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //this may need to move, may
 Adafruit_DCMotor *Left = AFMS.getMotor(1); //motor from port 1
 Adafruit_DCMotor *Right = AFMS.getMotor(2); //motor from port 2
 
-  int vSpeed = 30;        // MAX 255
-  int turn_speed = 200;    // MAX 255, for wheel on inside of turn 
-  int turn_delay = 10;
+  int vSpeed = 20;        // MAX 255
+  int turn_speed = 150;    // MAX 255, for wheel on inside of turn 
+  int turn_delay = 20;
   int n1=850;              // Right sensor value for yes/no line detection
-  int n2=850;               //Left sensor value for yes/no line detection
+  int n2=750;               //Left sensor value for yes/no line detection
 
 //Motor shield commands
 //myMotor->setSpeed(150); //speed set 0-255
@@ -49,10 +49,11 @@ if(right_sensor_state < n1 && left_sensor_state > n2)
   Left->run(FORWARD);
   Right->run(FORWARD);
 
-  Left->setSpeed(vSpeed); //speed set Left
+  Left->setSpeed(0); //speed set Left
   Right->setSpeed(turn_speed); //turn set Right
 
   
+  delay(turn_delay);
   }
 if(right_sensor_state > n1 && left_sensor_state < n2)
 {
@@ -61,10 +62,10 @@ if(right_sensor_state > n1 && left_sensor_state < n2)
   Left->run(FORWARD);
   Right->run(FORWARD);
 
-  Right->setSpeed(vSpeed); //speed set Right
+  Right->setSpeed(0); //speed set Right
   Left->setSpeed(turn_speed); //turn set Left
 
-//  delay(turn_delay);
+  delay(turn_delay);
   }
 
 if(right_sensor_state < n1 && left_sensor_state < n2)
@@ -92,7 +93,7 @@ if(right_sensor_state > n1 && left_sensor_state > n2)
   Right->setSpeed(0); //speed set Right, stop
   
   }
- 
+//  delay(3000);
 }
 //
 //void Serials() {
